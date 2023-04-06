581aa1f00dc211bb48953269c53478dd382e6ff9
Jasonm04 <109775152+Jasonm04@users.noreply.github.com>
2023-04-06 18:09:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+//Program for simple calculator
+let result;
+//Input to be used for calculation
+const num1 = parseFloat(prompt("Enter first number: "));
+const operator = prompt("Enter operator: ");
+const num2 = parseFloat(prompt("Enter second number: ");
+// Switch statement to do calculation
+switch (operator) {
+case '+' :
+    result = num1 + num2;
+    break;
+case '-' :
+    result = num1 - num2;
+    break;
+case '*' :
+    result = num1 * num2;
+    break;
+case '/' :
+    result = num1 / num2;
+    break;
+default :
+    console.log("Please enter a valid operator.");
+    break;
+}
